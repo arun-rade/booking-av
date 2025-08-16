a58502aa2254140ea7b63c57fa9051438b132d1b
arun.rade <arun.rade@gmail.com>
2025-08-16 09:05:27 +0530

    java code flight added

---
+++ src/main/java/com/example/booking_av/flight.java
@@ -0,0 +1,14 @@
+package com.example.booking_av;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please  book flights indigo ticket with 50% discount" ; }
+}
+
