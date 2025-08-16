ed3307345539381b30fb763f6db84a054861d199
arun.rade <arun.rade@gmail.com>
2025-08-16 08:44:53 +0530

    git

---
+++ src/main/java/com/example/booking_av/BookingAvApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_av;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booking_av/BookingAvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_av;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
